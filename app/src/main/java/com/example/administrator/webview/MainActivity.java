01eb2da086aa7e2e3deef695267c77d0014d555d
xz <xz2258@qq.com>
2018-05-16 14:54:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/webview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.administrator.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://172.31.0.164:8081/ejl-platform-web-api/user/signPage?token=ba3b25c30331655bccc14819314b92d28661");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+
+    }
+}
